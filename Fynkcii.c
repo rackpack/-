b73b506fa81e391611aedf5725d29e18d830fe4a
rackpack <32074731+rackpack@users.noreply.github.com>
2017-12-09 21:28:51 +0300

    Create Fynkcii.c

---
+++ Fynkcii.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int fynction(int NachSum,int vrem,int godovie)
+int main ()
+printf ("Введите процентную ставку и срок вклада");
+scanf ("%d %d",&godovie,&vrem);
+NachSum=100000;
+{
+int KonSum
+KonSum=NachSum*(1+ godovie/100)**vrem ;
+return (KonSum);
+}
+printf ("%d", KonSum);
+return 0;
